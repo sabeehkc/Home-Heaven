3d10c03137d20ef29a02ad44afb2be64b843f2f9
sabeehkc <wg457129@gmail.com>
2024-12-23 18:27:15 +0530

    client side started

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+      loggj
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "../styles/Register.scss"
+
+const RegisterPage = () => {
+  return (
+    <div className='register'>
+        <div className='register_content'>
+            <form className="register_content_form">
+                <input type="text" placeholder='First Name' name='firstname' required />
+                <input type="text"  placeholder='Last Name' name='lastname' required/>
+                <input type="email" placeholder='Email' name='email' required />
+                <input type="password" placeholder='Password' name='password' required />
+                <input type="password" placeholder='Confirm Password' name='confirmpassword' required />
+                <input id='image' type="file" name='profileImage' accept='image/*' style={{display: 'none'}} required />
+                <label htmlFor='image'>
+                    <img src="/assets/addImage.png" alt="add profile photo" />
+                    <p>Upload Your Photo</p>
+                </label>
+                <button type='submit'>REGISTER</button>
+            </form>
+            <a href="/login">Already have an account? Log In Here</a>
+        </div>
+    </div>
+  )
+}
+
+export default RegisterPage
